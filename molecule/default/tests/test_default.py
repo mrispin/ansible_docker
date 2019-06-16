413295bd5f4d5a3e583a220fffed7802ecb27fdd
Mark Rispin <mark@risp.in>
2019-06-16 18:35:11 +0100

    Added molecule tests and travis-ci integration

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,20 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_docker_installed(host):
+    docker = host.package("docker.io")
+    assert docker.is_installed
+
+def test_docker_group(host):
+    dockergroup = host.group("docker")
+    assert dockergroup.exists
+
+def test_docker_service(host):
+    dockerservice = host.service("docker")
+    assert dockerservice.is_enabled
+    assert dockerservice.is_running
